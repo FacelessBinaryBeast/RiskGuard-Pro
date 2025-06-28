f6d26c205ca99f4a3fb72c91ebb42b6497946ec7
Amit Rajegore <128823120+FacelessBinaryBeast@users.noreply.github.com>
2025-06-28 15:39:46 +0530

    Update main_updated.py
    removed api key

--- main_updated.py
+++ main_updated.py
@@ -17,7 +17,7 @@ import logging
 import google.generativeai as genai
 
 # Set API key
-os.environ["GEMINI_API_KEY"] = "AIzaSyDWGcViCdqOIAr0dCNYvjbL_3brTT6SEEw"
+os.environ["GEMINI_API_KEY"] = "Your_API_KEY"
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
